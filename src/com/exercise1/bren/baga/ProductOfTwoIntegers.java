d3c59523d58e00775ea40bf7c4ab814cb717a6d4
brentongalien981 <brentongalien981@gmail.com>
2022-02-19 11:45:47 -0500

    Initial commit

---
+++ src/com/exercise1/bren/baga/ProductOfTwoIntegers.java
@@ -0,0 +1,36 @@
+package com.exercise1.bren.baga;
+
+import java.util.Iterator;
+
+public class ProductOfTwoIntegers {
+
+	// 300726788 - Bren Baga
+	// Exercise1
+	public static void main(String[] args) {
+
+		int maxM = 10;
+		int maxN = 10;
+		
+		for (int i = 0; i < maxM; i++) {
+			
+			for (int j = 0; j < maxN; j++) {
+				
+				int p = getProduct(i, j);
+				String log = String.format("getProduct(%s, %s) ==> %s", i, j, p);
+				System.out.println(log);
+			}
+		}
+	}
+
+	
+	
+	public static int getProduct(int m, int n) {
+		if (n == 0) {
+			return 0;
+		} else {
+			return m + getProduct(m, n - 1);
+		}
+
+	}
+
+}
